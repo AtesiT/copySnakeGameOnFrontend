df164324fc567fcfab0540f0e67e4154afdb6ce6
AtesiT <atesit@mail.ru>
2023-01-21 21:30:36 +0300

    add moving snake and its velocity, add food

--- snake.js
+++ snake.js
@@ -8,6 +8,27 @@ let board;
 let context;
 
 
+// Голова змеи
+// Змея стартует с координат (5,5)
+let snakeX = blockSize * 5;
+let snakeY = blockSize * 5;
+
+
+// Скорость змеи
+let VelocityX = 0
+let VelocityY = 0
+
+
+
+// Еда
+let foodX;
+let foodY;
+
+
+
+
+
+
 window.onload = function() {
     board = document.getElementById('board');
     // Делаем "площадь" доски
@@ -15,14 +36,58 @@ window.onload = function() {
     board.width = cols * blockSize;
     context = board.getContext('2d');
     // Рисование на доске (сверху)
-    update();
+
+    placeFood();
+    document.addEventListener('keyup', changeDirection);
+    // update();
+    // Обновляется доска
+    setInterval(update, 1000/10); // Каждые 100 миллисекунд обновляется
 }
 
 
 // Для обновления доски
 function update() {
-    context.fillstyle = 'black';
+    context.fillStyle = 'black';
     context.fillRect(0, 0, board.width, board.height);
+    // Цвет змеи, её заливка
+    context.fillStyle = 'lime';
+    // Скорость увеличивается и положение меняется
+    snakeX+= VelocityX;
+    snakeY+= VelocityY;
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+    // Заливка еды
+    context.fillStyle = 'red';
+    context.fillRect(foodX, foodY, blockSize, blockSize);
+}
+
+// Меняет направление движения змеи
+function changeDirection(e) {
+    if (e.code == 'ArrowUp') {
+        VelocityX = 0;
+        VelocityY = -1;
+    } else if (e.code == 'ArrowDown') {
+        VelocityX = 0;
+        VelocityY = 1;
+    } else if (e.code == 'ArrowLeft') {
+        VelocityX = -1;
+        VelocityY = 0;
+    } else if (e.code == 'ArrowRight') {
+        VelocityX = 1;
+        VelocityY = 0;
+    }
+}
+
+
+
+
+
+
+// Размещение еды случайным образом
+function placeFood() {
+    foodX = Math.floor(Math.random() * cols) * blockSize
+    foodY = Math.floor(Math.random() * cols) * blockSize
+    // Случайное число от 0-1 * cols -> (0-19.9999) -> (0-19) * 25
+    
 }
 
 
